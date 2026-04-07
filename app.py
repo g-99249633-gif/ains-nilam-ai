1cbdcd05921341a8d8eb07c1f9ae673dc4821637
g-99249633-gif <g-99249633@moe-dl.edu.my>
2026-04-07 12:49:55 +0800

    app.py

--- app.py
+++ app.py
@@ -1,40 +1,5 @@
-# To run this code you need to install the following dependencies:
-# pip install google-genai
-
-import os
+import streamlit as st
 from google import genai
-from google.genai import types
 
-
-def generate():
-    client = genai.Client(
-        api_key=os.environ.get("GEMINI_API_KEY"),
-    )
-
-    model = "gemini-3-flash-preview"
-    contents = [
-        types.Content(
-            role="user",
-            parts=[
-                types.Part.from_text(text="""INSERT_INPUT_HERE"""),
-            ],
-        ),
-    ]
-    generate_content_config = types.GenerateContentConfig(
-        thinking_config=types.ThinkingConfig(
-            thinking_level="HIGH",
-        ),
-    )
-
-    for chunk in client.models.generate_content_stream(
-        model=model,
-        contents=contents,
-        config=generate_content_config,
-    ):
-        if text := chunk.text:
-            print(text, end="")
-
-if __name__ == "__main__":
-    generate()
-
-
+# Contoh cara panggil API Key dari Secrets Streamlit
+client = genai.Client(api_key=st.secrets["GEMINI_API_KEY"])
